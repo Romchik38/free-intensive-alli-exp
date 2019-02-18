d2f52c2e5d2b8b386382c4b38b019b1d5a98f425
Romchik38 <pomahehko.c@gmail.com>
2019-02-18 22:56:38 +0200

    День 1. Все сделано.

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ const cartWrapper = document.querySelector('.cart__wrapper'), //Корзина
     close = document.querySelector('.cart__close'),
     open = document.querySelector('#cart'), // Получаем по id="cart"
     goodsBtn = document.querySelectorAll('.goods__btn'), //Все кнопки товаров которые есть на странице
-    products = document.querySelector('.goods_item'),
+    products = document.querySelectorAll('.goods__item'),
     confirm = document.querySelector('.confirm'),
     badge = document.querySelector('.nav__badge'), //колво товаров в корзине
     totalCost = document.querySelector('.cart__total > span'), //span внутри div  Общая стоимость
@@ -25,3 +25,27 @@ function closeCart() {
 
 open.addEventListener('click', openCart); //Клик на добавить в корзину
 close.addEventListener('click', closeCart); //Клик на крестик для закрытия корзины
+
+goodsBtn.forEach(function (btn, i) {
+    btn.addEventListener('click', () => {
+        let item = products[i].cloneNode(true),             //клонируем товар
+            trigger = item.querySelector('button'),         //кнопка в клонируемом продукте
+            removeBtn = document.createElement('div'),      //новый блок для помещения в верстку
+            empty = cartWrapper.querySelector('.empty');    //сообщение в корзине что наша корзина пуста
+             
+            trigger.remove();       // удаляем добаваить в корзину
+            //Крестик
+            removeBtn.classList.add('goods__item-remove');
+            removeBtn.innerHTML = '&times';                 //комбинация которая добавляет крестик.
+            item.appendChild(removeBtn);                    //добавление крестика в клонируемый продукт
+
+            cartWrapper.appendChild(item);                  //добавление выбранного товара в корзину
+            if (empty) {
+                empty.remove();                             //удаление надписи ваша корзина пуста
+            }
+    })
+})
+
+
+
+//alert('code works');
